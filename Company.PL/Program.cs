55ecb04d9821a3f29893f7d0b64179e01d2a972b
osama0010 <osama.eidev@gmail.com>
2025-04-03 23:28:40 +0200

    .NET 6 Upgrade

--- Company.PL/Program.cs
+++ Company.PL/Program.cs
@@ -2,8 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Company.BLL.Interfaces;
+using Company.BLL.Repositories;
+using Company.DAL.Contexts;
+using Company.DAL.Models;
+using Company.PL.MappingProfiles;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,14 +24,70 @@ namespace Company.PL
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var Builder = WebApplication.CreateBuilder(args);
+
+            #region Configure Services that Allow Dependency Injection
+            Builder.Services.AddControllersWithViews();
+            Builder.Services.AddDbContext<CompanyAppDbContext>(options =>
+            {
+                options.UseSqlServer(Builder.Configuration.GetConnectionString("DefaultConnection"));
+            }); // Allow Dependency Injection
+
+            Builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>(); // Allow Dependency Injection for class DepartmentRepo
+            //services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            Builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            Builder.Services.AddAutoMapper(M => M.AddProfiles(new List<Profile>() { new EmployeeProfile(), new UserProfile(), new RoleProfile() }));
+
+            Builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+            })
+                        .AddEntityFrameworkStores<CompanyAppDbContext>()
+                        .AddDefaultTokenProviders(); ;
+
+            Builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(Options =>
+            {
+                Options.LoginPath = "Account/Login";
+                Options.AccessDeniedPath = "Home/Error";
+            });
+
+            #endregion
+
+            var app = Builder.Build(); //Kestrel
+
+            #region Configure Http Request Pipelines "Middlewares"
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Account}/{action=Login}/{id?}");
+            });
+            #endregion
+
+            app.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+
     }
 }
